7d074898e5a12115f924ffe02f79764a899bc010
Shannon-JXT <584901185@qq.com>
2019-08-26 21:47:02 +1000

    update for deploying

--- Country/admin.py
+++ Country/admin.py
@@ -1,8 +1,23 @@
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 from django.contrib import admin
 from .models import Country
 
+
+'''
+class StateResource(resources.ModelResource):
+    class Meta:
+        model = State
+        export_order = ('id', 'year', 'state_name', 'country_id', 'migrant_num')
+'''
 # Register your models here.
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ('id', 'country_name', 'create_time', 'last_updated_time')
     ordering = ("id",)
+'''
+@admin.register(State)
+class StateAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'year', 'state_name', 'country_name', 'migrant_num')
+    ordering = ('year',)
+'''
--- Country/urls.py
+++ Country/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.country_display, name='country_display'),
-    path('<int:country_pk>', views.country_detail, name='country_detail'),
 ]
 #http://localhost:8000/country/
